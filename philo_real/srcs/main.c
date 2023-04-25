bc60003e30ada7d5d70b25a7dbb7f28767fc3217
MasaDevs <110250805+MasaDevs@users.noreply.github.com>
2023-04-25 15:46:35 +0900

    pok

--- philo_real/srcs/main.c
+++ philo_real/srcs/main.c
@@ -4,6 +4,13 @@ int	set_info(t_info *info, int argc, char *argv[]);
 pthread_mutex_t	*make_mutex(const t_info info);
 t_philos *make_philos(const t_info info, pthread_mutex_t *mutex);
 pthread_t	*make_thread(t_info info, t_philos *philos);
+void	odd_philo_meal(t_philos *philo);
+void	even_philo_meal(t_philos *philo);
+int get_num_of_eat(t_philos *philo);
+void	set_philo_dead(t_philos *philo);
+int	is_philo_dead(t_philos *philo);
+void change_last_meal(t_philos *philo);
+void	print_philos(t_philos *philo, char *str);
 void	destructor(const t_info info, pthread_t *thread, t_philos *philos, pthread_mutex_t *mutex);
 void	*alloc(void *philos);
 
@@ -38,6 +45,7 @@ int	set_info(t_info *info, int argc, char *argv[])
 		info->num_of_eat = atoi(argv[5]);
 	else
 		info->num_of_eat = -1;
+	return (1);
 }
 
 pthread_mutex_t	*make_mutex(const t_info info)
@@ -116,7 +124,8 @@ void	odd_philo_meal(t_philos *philo)
 {
 	int		i;
 
-	while(i < philo->num_of_eat)
+	i = 0;
+	while(i < get_num_of_eat(philo) || get_num_of_eat(philo) < 0)
 	{
 		if (is_philo_dead(philo))
 			return ;
@@ -134,11 +143,41 @@ void	odd_philo_meal(t_philos *philo)
 		print_philos(philo, "is sleeping");
 		usleep(philo->time_to_sleep * 1000);
 		print_philos(philo, "is thinking");
-		if (0 < philo->num_of_eat)
+		if (0 < get_num_of_eat(philo))
 			i++;
 	}
 	print_philos(philo, "died");
-	philo->dead = 1;
+	set_philo_dead(philo);
+}
+
+void	even_philo_meal(t_philos *philo)
+{
+	int		i;
+
+	i = 0;
+	while(i < get_num_of_eat(philo) || get_num_of_eat(philo) < 0)
+	{
+		if (is_philo_dead(philo))
+			return ;
+		while (pthread_mutex_lock(philo->left_hand) != 0)
+			;
+		print_philos(philo, "has taken a fork");
+		while(pthread_mutex_lock(philo->right_hand) != 0)
+			;
+		change_last_meal(philo);
+		print_philos(philo, "is eating");
+		usleep(philo->time_to_eat * 1000);
+		change_last_meal(philo);
+		pthread_mutex_unlock(philo->right_hand);
+		pthread_mutex_unlock(philo->left_hand);
+		print_philos(philo, "is sleeping");
+		usleep(philo->time_to_sleep * 1000);
+		print_philos(philo, "is thinking");
+		if (0 < get_num_of_eat(philo))
+			i++;
+	}
+	print_philos(philo, "died");
+	set_philo_dead(philo);
 }
 
 int	is_philo_dead(t_philos *philo)
@@ -154,12 +193,51 @@ int	is_philo_dead(t_philos *philo)
 	return (flag);
 }
 
+int get_num_of_eat(t_philos *philo)
+{
+	int num_of_eat;
+
+	while(pthread_mutex_lock(&(philo->status)) != 0)
+		;
+	num_of_eat = philo->num_of_eat;
+	pthread_mutex_unlock(&(philo->status));
+	return (num_of_eat);
+}
+
+void	set_philo_dead(t_philos *philo)
+{
+	while(pthread_mutex_lock(&(philo->status)) != 0)
+		;
+	philo->dead = 1;
+	pthread_mutex_unlock(&(philo->status));
+}
+
 void change_last_meal(t_philos *philo)
 {
 	while (pthread_mutex_lock(&(philo->status)) != 0)
 		;
 	gettimeofday(&(philo->last_meal), NULL);
 	pthread_mutex_unlock(&(philo->status));
+}
+
+void	print_philos(t_philos *philo, char *str)
+{
+	struct timeval	tp;
+
+	while (pthread_mutex_lock(&(philo->status)) != 0)
+		;
+	if(philo->dead)
+		return ;
+	gettimeofday(&tp, NULL);
+	printf("%ld %d %s\n", tp.tv_sec * 1000 + tp.tv_usec / 1000, philo->philos_id, str);
+	if (!strcmp(str, "is eating"))
+		gettimeofday(&(philo->last_meal), NULL);
+	pthread_mutex_unlock(&(philo->status));
+}
+
+int	monitor(t_philos *philo)
+{
+
 }
 
 void	destructor(const t_info info, pthread_t *thread, t_philos *philos, pthread_mutex_t *mutex)
@@ -180,6 +258,13 @@ void	destructor(const t_info info, pthread_t *thread, t_philos *philos, pthread_
 		i++;
 	}
 }
+
+#include <libc.h>
+__attribute__((destructor))
+static void destructor() {
+    system("leaks -q a.out");
+}
+
 //malloc
 //mutex
 //thread
